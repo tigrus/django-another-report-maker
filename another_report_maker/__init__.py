cec896752fb2fbc0e0db95a280252958c7e72737
Nikolay Fominykh <nikolayfn@gmail.com>
2016-06-13 18:10:37 +0300

    converted into package

---
+++ another_report_maker/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+__title__ = 'another_report_maker'
+__version__ = '0.1.0'
+__author__ = 'Nikolay Fominykh'
+__license__ = 'MIT'
+
+# Version synonym
+VERSION = __version__
